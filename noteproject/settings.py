05cc8ebbb73623acf13bb585f3a9dc52bfd0e12c
wyh <1398838410@qq.com>
2022-03-29 18:01:37 +0800

    settings changed

--- noteproject/settings.py
+++ noteproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.wudaonote.com']
 
 
 # Application definition
@@ -130,5 +130,3 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.wudaonote.com']
